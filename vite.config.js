48ba5c494b36eff3cd58214978e591dc9b2afc13
Mufeed <mufeedkm010@gmail.com>
2024-08-23 09:59:06 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Employee-form-with-Reactjs-using-HOOKS"
+  base:"/Employee-form-with-Reactjs-using-HOOKS",
   plugins: [react()],
 })
